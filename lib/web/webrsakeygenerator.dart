d9cf98bc7eab1fb7059e3b0956eac85844dba64f
Nils Berghold <nberghold@gsd.software>
2024-11-28 10:09:47 +0100

    convert web key to AsymmetricKeyPair

--- lib/web/webrsakeygenerator.dart
+++ lib/web/webrsakeygenerator.dart
@@ -1,9 +1,12 @@
+import 'dart:convert';
 import 'dart:typed_data';
 // ignore: avoid_web_libraries_in_flutter
 import 'dart:js_util' as js_util;
+import 'package:encryption/extension.dart';
 import 'package:encryption/web/webgeneratekeyoptions.dart';
 import 'package:encryption/web/webhashalgorithm.dart';
 import 'package:js/js.dart';
+import 'package:pointycastle/export.dart';
 
 // Zugriff auf `window.crypto.subtle` in JavaScript
 @JS('window.crypto.subtle')
@@ -11,19 +14,70 @@ external dynamic get subtle;
 
 class WebRSAKeyGenerator {
   // Schlüssel generieren
-  static Future<dynamic> generateRSAKeys() async {
+  static Future<AsymmetricKeyPair<RSAPublicKey, RSAPrivateKey>> generateRSAKeys() async {
     final options = GenerateKeyOptions(
       name: 'RSA-OAEP',
       modulusLength: 2048,
       publicExponent: Uint8List.fromList([0x01, 0x00, 0x01]),
       hash: HashAlgorithm(name: 'SHA-256'),
     );
-    return js_util.promiseToFuture(
+
+    dynamic keypair = js_util.promiseToFuture(
       js_util.callMethod(subtle, 'generateKey', [
         options,
         true,
         ['encrypt', 'decrypt']
       ]),
     );
+
+    final publicKey = js_util.getProperty(keypair, 'publicKey');
+    final privateKey = js_util.getProperty(keypair, 'privateKey');
+
+    final rsaPublicKeyPEM = await _exportPublicKey(publicKey);
+    final rsaPrivateKeyPEM = await _exportPrivateKey(privateKey);
+
+    RSAPublicKey rsaPublicKey = rsaPublicKeyPEM.parsePublicKeyFromPem();
+    RSAPrivateKey rsaPrivateKey = rsaPrivateKeyPEM.parsePrivateKeyFromPem();
+
+    return AsymmetricKeyPair<RSAPublicKey, RSAPrivateKey>(
+          rsaPublicKey, rsaPrivateKey);
+  }
+
+  static Future<String> _exportPublicKey(dynamic publicKey) async {
+    // Exportiere den Schlüssel im SPKI-Format
+    final spkiKey = await js_util.promiseToFuture(
+      js_util.callMethod(subtle, 'exportKey', ['spki', publicKey])
+    );
+
+    // Base64-kodiere die Binärdaten
+    final base64Key = base64Encode(Uint8List.fromList(spkiKey as List<int>));
+
+    // Füge die PEM-Header und -Footer hinzu
+    final pemKey = [
+      '-----BEGIN PUBLIC KEY-----',
+      ...RegExp('.{1,64}').allMatches(base64Key).map((m) => m.group(0)!), // Zeilenumbrüche
+      '-----END PUBLIC KEY-----'
+    ].join('\n');
+
+    return pemKey;
+  }
+
+  static Future<String> _exportPrivateKey(dynamic privateKey) async {
+    // Exportiere den Schlüssel im PKCS8-Format
+    final pkcs8Key = await js_util.promiseToFuture(
+      js_util.callMethod(subtle, 'exportKey', ['pkcs8', privateKey])
+    );
+
+    // Base64-kodiere die Binärdaten
+    final base64Key = base64Encode(Uint8List.fromList(pkcs8Key as List<int>));
+
+    // Füge die PEM-Header und -Footer hinzu
+    final pemKey = [
+      '-----BEGIN PRIVATE KEY-----',
+      ...RegExp('.{1,64}').allMatches(base64Key).map((m) => m.group(0)!), // Zeilenumbrüche
+      '-----END PRIVATE KEY-----'
+    ].join('\n');
+
+    return pemKey;
   }
 }
